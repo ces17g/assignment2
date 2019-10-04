26a9b415665d68a2cfcd4e16af003bfbcc51463a
ces17g <54723379+ces17g@users.noreply.github.com>
2019-10-04 09:08:02 -0400

    Created Files : Speedboat.cpp Speedboat.h Updated Files : Boat.cpp Boat.h Changed Boat.* to Speedboat.* to clarify that class Boat (now Speedboat) inherits PoweredVehicle Updated Speedboat.cpp to display both engine count and size

---
+++ Speedboat.cpp
@@ -0,0 +1,55 @@
+// Created by Chris Santos on 10/4/19.
+
+#include "Speedboat.h"
+
+Speedboat::Speedboat() {
+    myEngineSize = "unknown";
+    setBrand("Custom");
+    setModel("VTx");
+}
+
+Speedboat::Speedboat(string brand, string model, string fuelType, string engineSize, int engineCount=1) {
+    setBrand(brand);
+    setModel(model);
+    setFuelType(fuelType);
+    setEngineSize(engineSize);
+}
+
+Speedboat::~Speedboat() = default;
+
+int Jet::getEngineCount() {
+    return numberOfEngines;
+}
+
+void Jet::setNumberOfEngines(int engineCount) {
+    if ( (engineCount > 0) && (engineCount < 6) ) { numberOfEngines=engineCount; }
+}
+
+string Speedboat::getEngineSize() {
+    return myEngineSize;
+}
+
+void Speedboat::setEngineSize(string engineSize) {
+    if (engineSize == "unknown" || engineSize == "small" ||
+        engineSize == "medium" || engineSize == "grande") {
+        myEngineSize = engineSize;
+    } else {
+        myEngineSize = "unknown";
+    }
+
+}
+
+double Speedboat::mileageEstimate(double time) {
+    srand( time(0) );
+    double mileage = (rand()%10 + 5) * time;
+    if (engineCount > 2)
+        mileage *= 1.055;
+    if (fuelType == "electricity")
+	mileage *= 1.005;
+    return mileage;
+}
+
+string Speedboat::toString() {
+    return "-> Speedboat\n" + PoweredVehicle::toString() + "\n\tEngine Count: " +
+           getEngineCount() + "\n\tEngine Size: " + getEngineSize();
+}
---
+++ Speedboat.h
@@ -0,0 +1,32 @@
+// Created by Chris Santos on 10/4/19.
+
+#include <cstdlib>
+#include <ctime>
+#ifndef DRIVINGSIMULATOR_SPEEDBOAT_H
+#define DRIVINGSIMULATOR_SPEEDBOAT_H
+
+#include "PoweredVehicle.h"
+
+class Speedboat : public PoweredVehicle {
+
+private:
+    string myEngineSize;
+    int numberOfEngines;
+
+public:
+    Speedboat();
+
+    explicit Speedboat(string brand, string model, string fuelType, 
+        string engineSize, int engineCount);
+
+    virtual ~Speedboat();
+    int getEngineCount();
+    void setNumberOfEngines(int engineSize);
+    string getEngineSize();
+    void setEngineSize(string engineSize);
+    virtual double mileageEstimate(double time);
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_SPEEDBOAT_H
